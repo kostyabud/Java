68aeae5acbc806aa046b8adb7213615d35eb18ec
Konstantin Budko <jactator63@gmail.com>
2020-12-06 11:26:22 +0300

    Done task 3 from Main

--- src/com/JavaFuddamentals/MainTask/Main.java
+++ src/com/JavaFuddamentals/MainTask/Main.java
@@ -1,5 +1,6 @@
 package com.JavaFuddamentals.MainTask;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +13,18 @@ public class Main {
         String name = in.next();
         System.out.println("Hello, " + name);
 
+        System.out.print("какое количество чисел вывести?: "); //Вывод заданного количества чисел
+        int num = in.nextInt();
+        int[] arr = new int [num];
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {             // без прехода на новую строку
+            arr[i] = random.nextInt(10000) - 1000;
+            System.out.print(arr[i] + "\t");
+        }
+        for (int i = 0; i < arr.length; i++) {             //с переходом
+            arr[i] = random.nextInt(10000) -1000;
+            System.out.println(arr[i]);
+        }
 
 
     }
