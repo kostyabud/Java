ca749424eb7fffe631f798849e77a7a4e8e9b281
Konstantin Budko <jactator63@gmail.com>
2020-12-06 10:59:26 +0300

    Done task 2 from Main

--- src/com/JavaFuddamentals/MainTask/Main.java
+++ src/com/JavaFuddamentals/MainTask/Main.java
@@ -4,10 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
-        Scanner in = new Scanner (System.in);
+        for (int i = args.length -1; i>=0; i--) {  //Аргументы командной строки в обрятном порядке
+            System.out.println(args[i]);
+        }
+        Scanner in = new Scanner (System.in); //Приветствие пользователя при вводе через консоль
         System.out.print("Enter you name: ");
         String name = in.next();
         System.out.println("Hello, " + name);
+
+
 
     }
 }
