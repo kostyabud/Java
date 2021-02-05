29c080df869ac44f294088c1eb9c074cd334b39c
Konstantin Budko <jactator63@gmail.com>
2021-02-05 17:47:22 +0300

    Done main i\o task

--- src/com/java_io/Element.java
+++ src/com/java_io/Element.java
@@ -3,6 +3,7 @@ package com.java_io;
 import java.io.*;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Element {
         public final String indent;
@@ -21,10 +22,11 @@ public class Element {
 
         public static void displayAllCatalogAndFiles(File directories){
             File dir = new File(String.valueOf(directories));
+            int countDirectories = 0;
             try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("e:/Homework/Java/data/name.txt")))) {
                 if (!dir.exists() || !dir.isDirectory())
                 {
-                    writer.println("Incorrect directory name");
+                    System.err.println("Incorrect directory name");
                 }
                 Deque<Element> stack = new ArrayDeque<>();
                 stack.add(new Element("|-", dir));
@@ -37,13 +39,44 @@ public class Element {
                         File[] files = element.file.listFiles();
                         for (int i = files.length - 1; i >=0; i--)
                         {
-                            stack.add(new Element(element.indent + "-- ", files[i]));
+                            stack.add(new Element(element.indent + "  ", files[i]));
                         }
                     }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        public static void readInformationFromFile(String file){
+            int countFile = 0;
+            int countDir = 0;
+            int allLength = 0;
+            try(FileReader reader = new FileReader(String.valueOf(file));
+                BufferedReader bufferedReader =  new BufferedReader(reader)) {
+                String line = null;
+               while((line = bufferedReader.readLine()) != null){
+                   if(line.endsWith(".txt")){
+                       countFile++;
+                       StringTokenizer str = new StringTokenizer(line);
+                       while (str.hasMoreTokens()){
+                           str.nextToken().length();
+                           allLength += str.nextToken().length();
+                       }
+                   } else {
+                       countDir++;
+                   }
+               }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int averageFileInCatalog = countFile / countDir;
+            allLength = allLength / countFile;
+            System.out.println("Файлов = " + countFile + "\n " + "Папок = " + countDir
+                    + "\n " + "Среднее количество файлов в папке = " + averageFileInCatalog
+                    + "\n " + "Среднее длина названия файла = " + allLength);
         }
 }
 
--- src/com/java_io/Main.java
+++ src/com/java_io/Main.java
@@ -1,23 +1,19 @@
 package com.java_io;
 
-import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+
+import java.io.File;
 
 import static com.java_io.Element.displayAllCatalogAndFiles;
+import static com.java_io.Element.readInformationFromFile;
 
 public class Main {
     public static void main(String[] args)
     {
         String directories = "";
-        for (int i = 0; i < args.length; i++) {
-            directories += args[i];
-
+        for (String arg : args) {
+            directories += arg;
         }
-        displayAllCatalogAndFiles(new File(directories));
-
-
-
+//      displayAllCatalogAndFiles(new File(directories));
+        readInformationFromFile(directories);
     }
 }
